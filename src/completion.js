c8762abcc61fd9fc02cf5ae0bf25d0191501e0cb
elihe999 <eli.he999@hotmail.com>
2019-09-23 18:23:15 +0800

    <chore>Update extension

--- src/completion.js
+++ src/completion.js
@@ -2,9 +2,6 @@ const vscode = require('vscode');
 const util = require('./util');
 
 /**
- * 自动提示实现，这里模拟一个很简单的操作
- * 当输入 this.dependencies.xxx时自动把package.json中的依赖带出来
- * 当然这个例子没啥实际意义，仅仅是为了演示如何实现功能
  * @param {*} document 
  * @param {*} position 
  * @param {*} token 
@@ -12,23 +9,29 @@ const util = require('./util');
  */
 function provideCompletionItems(document, position, token, context) {
     const line        = document.lineAt(position);
-    const projectPath = util.getProjectPath(document);
 
-    // 只截取到光标位置为止，防止一些特殊情况
     const lineText = line.text.substring(0, position.character);
-    // 简单匹配，只要当前光标前的字符串为`this.dependencies.`都自动带出所有的依赖
-    if(/(^|=| )\w+\.dependencies\.$/g.test(lineText)) {
-        const json = require(`${projectPath}/package.json`);
-        const dependencies = Object.keys(json.dependencies || {}).concat(Object.keys(json.devDependencies || {}));
-        return dependencies.map(dep => {
-            // vscode.CompletionItemKind 表示提示的类型
+    vscode.window.showInformationMessage(`111 ${/API:/.test(lineText)}`);
+    
+    if(/API:/.test(lineText)) {
+        const test = Object.keys({
+            "Key": "1222",
+            "Capture": "1122",
+            "Init": "",
+            "SoftKey": "",
+            "Config": "",
+            "Reboot": "",
+            "Reset": "",
+            "Provision": "",
+            "Get": ""
+        });
+        return test.map(dep => {
             return new vscode.CompletionItem(dep, vscode.CompletionItemKind.Field);
         })
     }
 }
 
 /**
- * 光标选中当前自动补全item时触发动作，一般情况下无需处理
  * @param {*} item 
  * @param {*} token 
  */
@@ -37,9 +40,8 @@ function resolveCompletionItem(item, token) {
 }
 
 module.exports = function(context) {
-    // 注册代码建议提示，只有当按下“.”时才触发
     context.subscriptions.push(vscode.languages.registerCompletionItemProvider('gsate', {
         provideCompletionItems,
         resolveCompletionItem
-    }, '.'));
+    }, ':'));
 };
--- src/extension.js
+++ src/extension.js
@@ -6,19 +6,19 @@ const vscode = require('vscode');
  */
 exports.activate = function(context) {
     console.log(vscode);
-    require('./helloword')(context); // helloworld
-    require('./test-command-params')(context); // 测试命令参数
-    require('./test-menu-when')(context); // 测试菜单when命令
-    require('./jump-to-definition')(context); // 跳转到定义
+    // require('./helloword')(context); // helloworld
+    // require('./test-command-params')(context); // 测试命令参数
+    // require('./test-menu-when')(context); // 测试菜单when命令
+    // require('./jump-to-definition')(context); // 跳转到定义
     require('./completion')(context); // 自动补全
     require('./hover')(context); // 悬停提示
-    require('./webview')(context); // Webview
-    require('./welcome')(context); // 欢迎提示
-    require('./other')(context); // 其它杂七杂八演示代码
+    // require('./webview')(context); // Webview
+    // require('./welcome')(context); // 欢迎提示
+    // require('./other')(context); // 其它杂七杂八演示代码
 
-    const testFn = require('./test-require-function');
-    console.log(testFn); // vscode的日志输出不可靠，这里竟然会打印null？！
-    testFn(1, 2);
+    // const testFn = require('./test-require-function');
+    // console.log(testFn); // vscode的日志输出不可靠，这里竟然会打印null？！
+    // testFn(1, 2);
 
     // 自动提示演示，在dependencies后面输入.会自动带出依赖
     // this.dependencies.
